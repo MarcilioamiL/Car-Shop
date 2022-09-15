6a4112798a8cddaea45921b3a1e36dc39a164b90
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-09-15 20:58:17 -0300

    test: test create service

--- src/tests/unit/services/carServicesTest.test.ts
+++ src/tests/unit/services/carServicesTest.test.ts
@@ -1,19 +1,36 @@
 import * as sinon from 'sinon';
 import chai from 'chai';
+import { ZodError } from 'zod';
 const { expect } = chai;
+import CarModel from '../../../models/carModel';
+import CarService from '../../../services/CarService';
+import { carMock, carMockWithId, invalid } from '../mocks/carMocks'
+import { ErrorTypes } from '../../../utils/Erros';
 
-describe('Sua descrição', () => {
+describe('Car service test', () => {
+	const carModel = new CarModel();
+	const carService = new CarService(carModel);
 
-  before(async () => {
-    sinon
-      .stub()
-      .resolves();
-  });
+	beforeEach(() => {
+		sinon.stub(carModel, 'create').resolves(carMockWithId);
+	});
 
-  after(()=>{
-    sinon.restore();
-  })
+	afterEach(() => {
+		sinon.restore()
+	});
 
-  it('', async () => {});
+	describe('Create Car', () => {
+		it('Success', async () => {
+			const carCreated = await carService.create(carMock);
+			expect(carCreated).to.be.deep.equal(carMockWithId);
+		});
 
+		it('Failure', async () => {
+			try {
+				await carService.create('object' as any);
+			} catch (error) {
+				expect(error).to.be.instanceOf(ZodError);
+			}
+		});
+	});
 });
