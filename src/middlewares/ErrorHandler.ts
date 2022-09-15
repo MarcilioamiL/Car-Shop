f74fadd708b136788de1fac3703abd8edc34e49d
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-09-15 11:50:05 -0300

    feat: error handler

---
+++ src/middlewares/ErrorHandler.ts
@@ -0,0 +1,21 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { ErrorTypes, errorCatalog } from '../utils/Erros';
+
+const errorHandler: ErrorRequestHandler = (err: Error | ZodError, _req, res, _next) => {
+  if (err instanceof ZodError) {
+    return res.status(400).json({ message: err.issues[0].message });
+  }
+
+  const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+  
+  const mappedError = errorCatalog[messageAsErrorType];
+  if (mappedError) {
+    const { message, httpStatus } = mappedError;
+    return res.status(httpStatus).json({ message });
+  }
+
+  return res.status(500).json({ message: 'Internal Server Error' });
+};
+
+export default errorHandler;
