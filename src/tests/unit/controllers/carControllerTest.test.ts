1ad360402a0d2335a6e986aacb646bb50e3422e1
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-09-20 15:07:37 -0300

    feat: carControllerTest redone, update, delete

--- src/tests/unit/controllers/carControllerTest.test.ts
+++ src/tests/unit/controllers/carControllerTest.test.ts
@@ -20,6 +20,10 @@ describe('Car Controller test', () => {
   beforeEach(() => {
     sinon.stub(carService, 'create').resolves(carMock);
     sinon.stub(carService, 'read').resolves(carMockWithIdAndArry);
+    sinon.stub(carService, 'readOne').resolves(carMockWithId);
+    
+		sinon.stub(carService, 'update').resolves(carMockWithId);
+		sinon.stub(carService, 'delete').resolves();
 
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
@@ -46,6 +50,43 @@ describe('Car Controller test', () => {
 
       expect((res.status as sinon.SinonStub).calledWith(200)).to.be.equal(true);
       expect((res.json as sinon.SinonStub).calledWith(carMockWithIdAndArry)).to.be.equal(true);
+    });
+  });
+
+  describe('test ReadOne', () => {
+		it('Success', async () => {
+      req.params = { id: carMockWithId._id };
+			await carController.readOne(req, res);
+
+			expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMockWithId)).to.be.true;
+		});
+
+		it('return status 400', async () => {
+      req.params = { id: '1' };
+			await carController.readOne(req, res);
+
+			expect((res.status as sinon.SinonStub).calledWith(400)).to.have.ownPropertyDescriptor;
+		});
+	});
+
+  describe('test Update', () => {
+    it('Success', async () => {
+      req.params = { id: carMockWithId._id };
+      req.body = carMockWithId;
+      await carController.update(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMockWithId)).to.be.true;
+    });
+  });
+
+  describe('test Delete', () => {
+    it('Return status 500', async () => {
+      req.params = { id: carMockWithId._id };
+      await carController.delete(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(500)).to.have.ownPropertyDescriptor;
     });
   });
 });
